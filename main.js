ffefb85f8cd0e847ac1645d62d87ed1d34943aa2
jamcry <jamcry@hotmail.com>
2019-07-07 15:23:22 +0300

    Complete fetch,set weather data feature

--- main.js
+++ main.js
@@ -1,7 +1,58 @@
+class UI {
+  constructor() {
+    this.cityForm = document.querySelector('#city-search');
+    this.cityInput = document.querySelector('#city-name');
+    this.btnSearch = document.querySelector('#btn-city-search');
+    this.weatherDiv = document.querySelector('.weather-info');
+    this.cityForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      setCityInfo(this.cityInput.value, this.weatherDiv);
+    });
+  }
+}
+
+async function setCityInfo(city, weatherDiv) {
+  const weatherData = await getCityInfo(city);
+  const location = weatherDiv.querySelector('#location');
+  const weather = weatherDiv.querySelector('#weather');
+  const humidity = weatherDiv.querySelector('#humidity');
+  if (weatherData.cityFound) {
+    location.textContent = `${weatherData.city}, ${weatherData.country}`;
+    weather.textContent = `${weatherData.weather.temp} C`;
+    humidity.textContent = `Humidity: ${weatherData.weather.humidity}%`;
+  } else {
+    location.textContent = "CITY NOT FOUND!";
+  }
+  
+}
+
 async function getCityInfo(city) {
   const API_KEY = 'API_KEY_HERE';
-  const targetURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}`;
+  const units = 'metric';
+  const targetURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${API_KEY}`;
   let response = await fetch(targetURL);
   let weatherData = await response.json();
-
+  if (weatherData.cod === '404') {
+    return { cityFound: false }
+  }
+  return {
+    cityFound: true,
+    city: weatherData.name,
+    country: weatherData.sys.country,
+    coord: {
+      lon: weatherData.coord.lon,
+      lat: weatherData.coord.lat
+    },
+    weather: {
+      status: weatherData.weather.main,
+      description: weatherData.weather.description,
+      temp: weatherData.main.temp,
+      humidity: weatherData.main.humidity,
+      minTemp: weatherData.main.temp_min,
+      maxTemp: weatherData.main.temp_max,
+      windSpeed: weatherData.wind.speed
+    }
+  }
 }
+
+const ui = new UI();
